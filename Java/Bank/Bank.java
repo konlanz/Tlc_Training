ac9c7cad51139c191e61a70b33226a8ce097565f
Konlan Mikpe Zion <konlanmikpekoah.km@outlook.com>
2021-10-05 14:20:28 +0000

    working on the bank account methio

--- Java/Bank/Bank.java
+++ Java/Bank/Bank.java
@@ -3,12 +3,22 @@ package Bank;
 public class Bank {
     private String Owner;
     private double balance;
-    Public Bank
+    public Bank(String owner, double StartingBalance){
+        this.Owner = owner;
+        this.balance = StartingBalance;
+    }
     public String getOwner() {
         return Owner;
     };
     public double getBalance() {
         return balance;
     }
-    
+    public double depositBalance(double amt) {
+        if(amt > 0){
+            this.balance =this.balance + amt;
+            return amt;
+        }
+        return 0;
+
+    }
 }
