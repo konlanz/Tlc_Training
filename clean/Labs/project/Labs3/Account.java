3123d2cd0ffbe81b234ef2fb2a2167ed392f47ed
Konlan Mikpe Zion <konlanmikpekoah.km@outlook.com>
2021-10-08 16:10:45 +0000

    looks great

---
+++ clean/Labs/project/Labs3/Account.java
@@ -0,0 +1,12 @@
+public class Account {
+    private double TotalTrade;
+    public void setTotalTrade(double TotalTrade) {}{
+        this.TotalTrade = TotalTrade;
+    }
+    public double GetTotalTrade(){
+        return this.TotalTrade;
+    }
+    
+
+
+}
