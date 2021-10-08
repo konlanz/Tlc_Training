eacf019c518304f051ad6d7dc4db927c3f448aed
Konlan Mikpe Zion <konlanmikpekoah.km@outlook.com>
2021-10-08 19:33:26 +0000

    abtract classes

---
+++ clean/Labs/project/Labs3/abtaract/Trade.java
@@ -0,0 +1,11 @@
+package abtaract;
+
+public abstract class Trade {
+    abstract public void calcDividend();
+    public class bondTrades{
+
+    }
+    public class fundTrades{
+        
+    }
+}
