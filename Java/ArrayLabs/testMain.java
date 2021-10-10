fd9275a423ddfc05575047eb33f79fe45c2fa5ec
konlan mikpe zion <konlanmikpekoah.km@outlook.com>
2021-10-10 13:53:02 +0000

    done

---
+++ Java/ArrayLabs/testMain.java
@@ -0,0 +1,52 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+public class testMain {
+    
+    public static void arrayTwo(){
+        Integer[] randArray = new Integer[6];
+        Set<Integer> set = new HashSet<Integer>();
+        
+        int count = 0;
+        while(count < randArray.length){
+            int max = 49, min = 1;
+            int rand = (int) (Math.random()*(max-min+1)+min);
+            set.add(rand);
+            count++;
+
+        }
+         randArray = set.toArray(randArray);
+         insertSortFunction(randArray);
+         Arrays.sort(randArray);
+        for (Integer i: randArray){
+            
+            System.out.println(i);
+        }
+        
+        
+    
+    }
+    //insert sort function
+    public static void insertSortFunction(Integer[] array){
+        int l = array.length;
+        for(int k =1; k < l; k++){
+            int key = array[k];
+            int i = k-1;
+            while ( (i > -1) && ( array [i] > key ) ) {  
+                array [i+1] = array [i];  
+                i--;  
+            }  
+            array[i+1] = key; 
+        }
+
+        
+    }
+    
+    @Test 
+    void test(){
+        Integer[] array;
+        Integer[] randArray;
+        assertEquals(array[0], randArray[0]);
+    }
+}
