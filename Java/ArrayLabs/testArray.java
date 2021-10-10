750ec0e0e20bf42fa22c9659f8c9c9b5378a16aa
konlan mikpe zion <konlanmikpekoah.km@outlook.com>
2021-10-10 10:11:40 +0000

    test done

--- Java/ArrayLabs/testArray.java
+++ Java/ArrayLabs/testArray.java
@@ -1,13 +1,13 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
-
-public class arrayOne {
-    public static void main(String[] args) {
+public class testArray {
+    @Test
+    void testArr(){
         int[] first = new int[10];
         for (int i = 0; i < first.length; i++){
             first[i] = i+1;
         }
-        System.out.println(first[9]);
+        assertEquals(first[8], 9);
+        
     }
 }
