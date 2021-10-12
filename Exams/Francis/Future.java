26f9c15e43e54b0fbc67c2c652189812faab7422
Konlan Mikpe Zion <konlanmikpekoah.km@outlook.com>
2021-10-12 12:49:22 +0000

    looking great

--- Exams/Francis/Future.java
+++ Exams/Francis/Future.java
@@ -1,5 +1,46 @@
 public class Future extends  Product{
     private String exchange, contractCode;
+    public String getExchange() {
+        return exchange;
+    }
+
+    public void setExchange(String exchange) {
+        this.exchange = exchange;
+    }
+
+    public String getContractCode() {
+        return contractCode;
+    }
+
+    public void setContractCode(String contractCode) {
+        this.contractCode = contractCode;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
     private int month, year;
     
+    public Future(String id, String exchange, String contractCode, int month, int year) {
+        super(id);
+        this.exchange = exchange;
+        this.contractCode = contractCode;
+        this.month = month;
+        this.year = year;
+        
+    }
+
 }
