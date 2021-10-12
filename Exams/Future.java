d535acf70853de5b3eb15749b47eabc4fea39aaa
Konlan Mikpe Zion <konlanmikpekoah.km@outlook.com>
2021-10-12 12:13:50 +0000

    looks great

---
+++ Exams/Future.java
@@ -0,0 +1,9 @@
+public class Future extends Product{
+    private String exchange, contractCode;
+    private int month, year;
+    public Future(String exchange, String contractCode){
+        this.exchange = exchange;
+        this.contractCode = contractCode;
+    }
+    
+}
