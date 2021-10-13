0e275c24af20586da90044cd3b8438c9b8b48abf
Konlan Mikpe Zion <konlanmikpekoah.km@outlook.com>
2021-10-13 16:03:52 +0000

    this lab 2 done

--- Kotlin/Lab2.kt
+++ Kotlin/Lab2.kt
@@ -32,7 +32,14 @@ fun withdraw(amount : Double){
     println("Please input your password to withdraw")
     var inputx  = readLine()!!
     if(pswd == inputx){
-        funds -= amount
+        if( amount> funds){
+            println("You have insuficent balance $funds")
+        }else{
+            funds -= amount
+            println("You have withdram $funds from your account")
+        }
+        
+
     }else{
         println("Invalid password")
     }
